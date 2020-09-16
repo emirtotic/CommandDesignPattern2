5f4bc41061cd6978f30e96574318188a13bc8d08
Emir Totic <59924947+emirtotic@users.noreply.github.com>
2020-09-16 15:19:23 +0200

    Add files via upload
    Command Design Pattern, example with multithreading.

---
+++ src/CommandDP/MainClass.java
@@ -0,0 +1,34 @@
+package CommandDP;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		final Algorithm algorithm = new Algorithm();
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				algorithm.produce();
+				
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				algorithm.consume();
+				
+			}
+		});
+
+		t1.start();
+		t2.start();
+		
+	}
+
+}
---
+++ src/CommandDP/Task.java
@@ -0,0 +1,15 @@
+package CommandDP;
+
+public class Task {
+
+	private int id;
+	
+	public Task(int id) {
+		this.id = id;
+	}
+	
+	public void solveProblem() {
+		System.out.println("Solving the problem with ID " + id);
+	}
+	
+}
